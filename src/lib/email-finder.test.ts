e122a5206d5df5fbc448434811e5413dddc29ec9
Jheintze <jheintze33@gmail.com>
2026-09-23 16:44:54 -0600

    add more tests

--- src/lib/email-finder.test.ts
+++ src/lib/email-finder.test.ts
@@ -4,4 +4,7 @@ describe("isValidEmail", () => {
   it("accepts a normal restaurant email", () => {
     expect(isValidEmail("info@restaurant.de")).toBe(true);
   });
+  it("accepts a Gmail address", () => {
+  expect(isValidEmail("restaurant@gmail.com")).toBe(true);
+});
 });
