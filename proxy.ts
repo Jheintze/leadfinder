5cc4800eee0c98bf6e70d388ddfa9d842a80a419
Jheintze <jheintze33@gmail.com>
2026-09-25 16:21:32 -0600

    change root proxy to redirect to login

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,26 @@
-import { type NextRequest } from "next/server";
-
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/lib/supabase/proxy";
 
 export async function proxy(request: NextRequest) {
-  return await updateSession(request);
+  const { user, response } = await updateSession(request);
+
+  const isLoginPage = request.nextUrl.pathname === "/login";
+
+  if (!user && !isLoginPage) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+
+    return NextResponse.redirect(url);
+  }
+
+  if (user && isLoginPage) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/";
+
+    return NextResponse.redirect(url);
+  }
+
+  return response;
 }
 
 export const config = {
